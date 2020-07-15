969068e1a1beabbabae02049b3d7af50f6f68991
zhai-jr <68054682+zhai-jr@users.noreply.github.com>
2020-07-15 20:18:46 +0800

    Update man.py

--- ch01/man.py
+++ ch01/man.py
@@ -1,1 +1,17 @@
-#
+# -*- coding:utf-8 -*-
+
+class Man:
+    def __init__(self, name):
+        self.name = name
+        print("Initialized")
+
+    def hello(self):
+        print("Hello " + self.name + "!")
+
+    def goodbye(self):
+        print("Goodbye " + self.name + "!")
+
+
+man = Man("David")
+man.hello()
+man.goodbye()
